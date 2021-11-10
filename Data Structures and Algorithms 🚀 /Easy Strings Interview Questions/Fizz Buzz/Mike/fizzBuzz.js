c73aa735b7e7b4984e01467c31f74a6d47687b36
Michael Fresson <mikefresson@hotmail.com>
2021-11-10 17:12:06 +0000

    fizzbuzz mike start

--- Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Fizz Buzz/Mike/fizzBuzz.js
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Fizz Buzz/Mike/fizzBuzz.js
@@ -2,7 +2,21 @@
 
 // FIZZBUZZ: take an array of numbers and return fizz if a multiple of three, buzz if a multiple of five, and fizzbuzz if a mutliple of both. 
 // i.e 6 = fizz, 10 = buzz, 30 = fizzbuzz.
+let start = 0
+const end = 1000
+const myArray = [];
 
-export function fizzBuzz(array) {
+do {
+  myArray.push(start);
+  start++;
+} while( myArray < 100 )
 
+
+setTimeout()
+console.log(myArray)
+
+function fizzBuzz(array) {
+  console.log("fizzbuzz")
 }
+
+fizzBuzz([1, 2, 3, 4, 5])
