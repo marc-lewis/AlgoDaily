83de4996f5b07ce14d9bb38dc671ffcb301f0765
Michael Fresson <mikefresson@hotmail.com>
2021-11-10 15:20:07 +0000

    added reversString.js

---
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Reverse A String/Mike/reverseString.js
@@ -0,0 +1,67 @@
+console.log("ok, you got this.");
+
+const string = "Steven Universe";
+
+
+//My First version
+
+
+// const reverseString = string => {
+//   console.log(`string to reverse = ${string}`);
+
+//   const array = Array.from(string);
+//   const length = array.length;
+//   const reveresedArray = [];
+  
+//   for (let i = 0; i < length; ++i) {
+//     let element = array.pop()
+//     reveresedArray.push(element)
+//   }
+  
+//   const reveresedString = reveresedArray.join('')
+
+//   return reveresedString
+  
+// }
+
+
+// console.log(`reversed string = ${reverseString(string)}`);
+
+
+// // Improved Version
+// const reverseString2 = string => {
+//   console.log(`string to reverse = ${string}`);
+
+//   const array = Array.from(string);
+//   const length = array.length;
+//   let newString ='';
+  
+//   for (let i = 0; i < length; ++i) {
+//     let element = array.pop()
+//     newString = newString + element;
+//   }
+
+//   return newString;
+  
+// }
+
+// console.log(`reversed string = ${reverseString2(string)}`);
+
+// Two Pointer Technique
+const reverseString3 = string => {
+  console.log(`string to reverse = ${string}`);
+
+  let stringArray = string.split("");
+  let start = 0;
+  let end = string.length - 1;
+
+  while (start <= end) {
+    const temp = stringArray[start];
+    stringArray[start] = stringArray[end];
+    stringArray[end] = temp;
+    start++;
+    end--;
+  }
+}
+
+console.log(`reversed string = ${reverseString3(string)}`);
