f6ddb9cc2a04ea4c5668123baa35e4fe600cd9e9
Marc Lewis <marc.lewis@flyt.io>
2021-11-16 13:46:43 +0000

    feat: add the solution for isAnagram from algoDaily

--- Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Is An Anagram/Marc/isAnagram.js
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Is An Anagram/Marc/isAnagram.js
@@ -1,5 +1,6 @@
 /**
  * Return a bool of whether strings are anagrams of each other
+ * (Marc's solution)
  * @param {*} str1
  * @param {*} str2
  */
@@ -13,4 +14,16 @@ export function isAnagram(str1, str2) {
     }
   }
   return true;
+}
+
+/**
+ * The solution from AlgoDaily
+ * Return a bool of whether strings are anagrams of each other
+ * @param {*} str1
+ * @param {*} str2
+ */
+ export function algoDailySolution(str1, str2) {
+  const str1Sorted = str1.toLowerCase().split("").sort().join('');
+  const str2Sorted = str2.toLowerCase().split("").sort().join('');
+  return str1Sorted === str2Sorted
 }
