7cd61825883d44f2c206f2692d08c3dd9ab269d7
Marc Lewis <marc.lewis@flyt.io>
2021-11-16 15:40:00 +0000

    fix: fix first solution in Marc/isPalindrome

--- Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Validate Palindrome/Marc/isPalindrome.js
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Validate Palindrome/Marc/isPalindrome.js
@@ -1,17 +1,21 @@
 /**
  * Return a bool of whether strings are anagrams of each other
- * (Marc's solution)
- * doesn't match for all strings?
+ * (Marc's solution 1)
  * @param {*} str1
  */
 // export function isPalindrome(str) {
-//   const normalisedStr = str.toLowerCase().replace(/ /g, '');
+//   const normalisedStr = str.toLowerCase().replace(/\s/g, '');
 //   const reversed = normalisedStr.split('').reverse().join('');
-//   return str === reversed;
+//   return normalisedStr === reversed;
 // }
 
+/**
+ * Return a bool of whether strings are anagrams of each other
+ * (Marc's solution 2)
+ * @param {*} str1
+ */
 export function isPalindrome(str) {
-  const normalisedStr = str.toLowerCase().replace(/ /g, '').split('');
+  const normalisedStr = str.toLowerCase().replace(/\s/g, '').split('');
   let head = 0;
   let tail = normalisedStr.length - 1;
   while(head < tail) {
