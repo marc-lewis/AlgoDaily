11d2747b71295d2eaff2fe0e7db7c8170d1f2ac2
Marc Lewis <marc.lewis@flyt.io>
2021-11-16 12:09:05 +0000

    feat: add fizzbuzz test for Marc

--- Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Fizz Buzz/Marc/fizzBuzz.js
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Fizz Buzz/Marc/fizzBuzz.js
@@ -4,5 +4,20 @@
  * @returns {string}
  */
 export function fizzBuzz(int) {
-  return int;
+  let result = [];
+  for(let i = 1; i <= int; i++) {
+    if(
+      i % 3 === 0
+      && i % 5 === 0
+    ) {
+      result.push('fizzbuzz');
+    } else if (i % 5 === 0) {
+        result.push('buzz');
+    } else if (i % 3 === 0) {
+        result.push('fizz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result.join('');
 }
