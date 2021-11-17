d8f53b6141f8ed90f4572420fa18a337e73d606d
Michael Fresson <michael.fresson@adamandeveddb.com>
2021-11-17 15:04:27 +0000

    anagram mike finished

--- Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Is An Anagram/Mike/isAnagram.js
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Is An Anagram/Mike/isAnagram.js
@@ -4,5 +4,13 @@
  * @param {*} str2
  */
 export function isAnagram(str1, str2) {
-
+  str1 = str1.toUpperCase();
+  str2 = str2.toUpperCase();
+  
+  for (let char1 of str1) {
+    if (str2.indexOf(char1) === -1) {
+      return false;
+    }
+  }
+  return true;
 }
