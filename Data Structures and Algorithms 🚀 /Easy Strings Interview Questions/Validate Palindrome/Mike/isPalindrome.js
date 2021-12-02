4a1ed4a97968e59ac98d836206f2e10cb7c28224
Michael Fresson <michael.fresson@adamandeveddb.com>
2021-12-02 10:42:54 +0000

    mike is palindrome

--- Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Validate Palindrome/Mike/isPalindrome.js
+++ Data Structures and Algorithms 🚀 /Easy Strings Interview Questions/Validate Palindrome/Mike/isPalindrome.js
@@ -3,5 +3,15 @@
  * @returns {boolean}
  */
 export function isPalindrome(str) {
-  return str;
+  const strForward = str.toLowerCase().replace(/\s/g, '').split("");
+  let head = 0;
+  let tail = strForward.length - 1;
+  while (head < tail) {
+    if (strForward[head] !== strForward[tail]) {
+      return false;
+    }
+    head++;
+    tail--;
+  }
+  return true;
 }
